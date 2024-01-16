beac01d62396c475a28404df7cdf16a58b57b76a
joshuabutler <butlerja.nhs@gmail.com>
2024-01-15 19:44:07 -0500

    create flights schema

---
+++ models/flight.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const flightsSchema = new Schema ({
+  airline: {
+    type: String, 
+    enum: ['American', 'Southwest', 'United']
+  }, 
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN'
+  }, 
+  flightNo: {
+    type: Number,
+    min: 10, 
+    max: 9999
+  },
+  departs: {
+    type: Date,
+    default: () => {
+      const today = new Date()
+      return new Date(today.setFullYear(today.getFullYear() + 1))
+    }
+  }
+  
+})
